6b5447ae39c3f723e55db2434c1a3a119b08f993
Mikael Vito <supervto@Mikaels-MacBook-Air.local>
2022-04-13 19:42:24 +0700

    setup login

--- index.js
+++ index.js
@@ -2,10 +2,32 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const users = [];
+
+users.push({ email: "mikaelvto@gmail.com", password: "abcde" });
+
 app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  users.push({
+    email: email,
+    password: password,
+  });
+  console.log(users);
+  res.redirect("/register");
 });
 
 app.get("/salam", (req, res) => {
